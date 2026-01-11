1c39684a3a98ba6c117f3791e6564d46b913e32e
tbot2026nm-latvia <tbot2026nm@gmail.com>
2026-01-11 06:29:51 +0500

    Fix bot startup and database init

--- bot.py
+++ bot.py
@@ -1,17 +1,32 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
-from handlers import start, register, admin
+from services.db import connect_db
+
+from handlers import start, register, admin, menu
+
 
 async def main():
-    bot = Bot(BOT_TOKEN)
+    # Bot
+    bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # Routers
     dp.include_router(start.router)
     dp.include_router(register.router)
     dp.include_router(admin.router)
+    dp.include_router(menu.router)
 
+    # Database
+    await connect_db()
+
+    print("✅ Database ulandi")
+    print("🤖 Bot ishga tushdi")
+
+    # Start polling
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
